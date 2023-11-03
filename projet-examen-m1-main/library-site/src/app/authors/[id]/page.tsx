4ed49d1c201d789959a286b770249bacd032e79b
clementgorse <72432474+clementgorse@users.noreply.github.com>
2023-11-03 18:39:07 +0100

    ajout liste des livres de l'auteur

--- projet-examen-m1-main/library-site/src/app/authors/[id]/page.tsx
+++ projet-examen-m1-main/library-site/src/app/authors/[id]/page.tsx
@@ -1,26 +1,35 @@
 'use client';
 
 import { useParams } from 'next/navigation';
+import { Card, CardContent, Typography, Grid } from '@mui/material';
+//import { useRouter } from 'next/router';
 import { FC, useEffect, useState } from 'react';
 import axios from 'axios';
-import { PlainAuthorModel } from '../../../models/author.model'; // Assurez-vous d'importer le modèle approprié pour l'auteur
+import { PlainAuthorModel, PlainBookModel } from '../../../models';
+import Link from '@mui/material/Link';
 
 const AuthorDetailsPage: FC = () => {
   const { id } = useParams();
   const [authorDetails, setAuthorDetails] = useState<PlainAuthorModel | null>(null);
+  const [authorBooks, setAuthorBooks] = useState<PlainBookModel[]>([]);
 
   useEffect(() => {
-    const fetchAuthorDetails = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get<PlainAuthorModel>(`http://localhost:3001/authors/${id}`);
-        setAuthorDetails(response.data);
+        // Récupérer les détails de l'auteur
+        const authorResponse = await axios.get<PlainAuthorModel>(`http://localhost:3001/authors/${id}`);
+        setAuthorDetails(authorResponse.data);
+
+        // Récupérer la liste des livres de l'auteur
+        const booksResponse = await axios.get<PlainBookModel[]>(`http://localhost:3001/books?authorId=${id}`);
+        setAuthorBooks(booksResponse.data);
       } catch (error) {
-        console.error('Erreur lors de la récupération des détails de l\'auteur :', error);
+        console.error('Erreur lors de la récupération des données :', error);
       }
     };
 
     if (id) {
-      fetchAuthorDetails();
+      fetchData();
     }
   }, [id]);
 
@@ -31,18 +40,38 @@ const AuthorDetailsPage: FC = () => {
   return (
     <div className="bg-gray-100 p-4 border border-gray-300 rounded-lg shadow-md text-center">
       <h1 className="text-2xl font-bold mb-2 text-gray-800">Author Details</h1>
-      <div className="flex flex-col justify-center items-center">
-        <div className="text-lg text-gray-600">
-          <span className="font-semibold">First Name:</span> {authorDetails.firstName}
-        </div>
-        <div className="text-lg text-gray-600">
-          <span className="font-semibold">Last Name:</span> {authorDetails.lastName}
-        </div>
-        {authorDetails.photoUrl && (
-          <img src={authorDetails.photoUrl} alt={`${authorDetails.firstName} ${authorDetails.lastName}`} className="rounded-fulloverflow-hidden w-29 h-29 mx-auto" />
-        )}
-        {/* Ajoutez d'autres détails de l'auteur ici */}
-      </div>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12} md={6}>
+          <Card className="transition duration-300 ease-in-out transform hover:scale-105">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {authorDetails.firstName} {authorDetails.lastName}
+              </Typography>
+              {authorDetails.photoUrl && (
+                <img src={authorDetails.photoUrl} alt={`${authorDetails.firstName} ${authorDetails.lastName}`} className="rounded-full overflow-hidden w-48 h-48 mx-auto mt-4" />
+              )}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card className="transition duration-300 ease-in-out transform hover:scale-105">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Books by {authorDetails.firstName} {authorDetails.lastName}
+              </Typography>
+              <ul className="list-disc mt-2">
+                {authorBooks.map((book) => (
+                  <li key={book.id} className="mb-2">
+                    <Link href={`/books/${book.id}`}>
+                      <a className="text-blue-500 hover:underline">{book.name}</a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 };
